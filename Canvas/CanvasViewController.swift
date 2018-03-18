40fe80ab71c4954f99cf73294dde4d4d46816cf8
Carlos Huizar-Valenzuela <carlos_huizar@Carloss-MacBook-Pro.local>
2018-03-18 16:25:40 -0700

    User can now pan faces from the try to the canvas

--- Canvas/CanvasViewController.swift
+++ Canvas/CanvasViewController.swift
@@ -15,6 +15,8 @@ class CanvasViewController: UIViewController {
     var trayDownOffset: CGFloat! = nil
     var trayUp: CGPoint!
     var trayDown: CGPoint!
+    var newlyCreatedFace: UIImageView!
+    var newlyCreatedFaceOriginalCenter: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -65,6 +67,30 @@ class CanvasViewController: UIViewController {
         }
             
         
+    }
+    @IBAction func didPanFace(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        if sender.state == .began
+        {
+            print("Gesture began")
+            var imageView = sender.view as! UIImageView
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            view.addSubview(newlyCreatedFace)
+            newlyCreatedFace.center = imageView.center
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+            
+        }
+        else if sender.state == .changed
+        {
+            print("Gesture is changing")
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+            
+        }
+        else if sender.state == .ended
+        {
+            
+        }
     }
     
 
